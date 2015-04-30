72ebe1afce6b12d116950551932d00d52d9a2ef0
stefania brusatin <bruss266@newschool.edu>
2015-04-30 16:27:49 -0400

    cleaner code

---
+++ scripts/app.js
@@ -0,0 +1,32 @@
+var x = 0;
+var vx = 0;
+var ax = 0;
+var player = document.getElementById('player');
+var hand = document.getElementById('hand');
+var scene = document.querySelector('.scene');
+
+if (typeof window.DeviceMotionEvent != 'undefined') {
+	window.ondevicemotion = function(e) {
+		ax = event.accelerationIncludingGravity.x * 5;
+	}
+
+	setInterval(function() {
+		vx += ax;
+		vx *= 0.98;
+		x += vx / 50;
+		var size = Math.min(screen.width, scene.offsetWidth) / 2 / 2.3;
+		if (x > size) {
+			x = size;
+			vx *= -1;
+		} else if (x < -size) {
+			x = -size;
+			vx *= -1;
+		}
+		player.style.marginLeft = (x / 10) + 'px';
+		hand.style.marginLeft = (x - 133) + 'px';
+	}, 25);
+}
+
+document.getElementById('about').onclick = function() {
+	document.body.classList.add('show-video');
+}
